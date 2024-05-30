63a0f9540957bda15210c8fb46964efb71b6803a
Fish-SWA <zhonghengyu2@gmail.com>
2024-05-30 12:18:27 -0400

    完善了串口发送

--- core/inc/usart.hpp
+++ core/inc/usart.hpp
@@ -1,11 +1,36 @@
+/*
+用于实现串口通讯打类：
+ TODO
+ 串口监听线程-监听串口是否在线&中断实现
+ 串口工具
+ serialWrite - 向串口发送数组
+ serialPrintf
+
+
+
+*/
+
+
+
 class Usart
 {
 private:
-    /* data */
+    int fd; //设备文件指针
+    enum{
+    SERIAL_OK = 0,
+    SERIAL_FAIL
+};
 public:
-    // usart(/* args */);
-    // ~usart();
     const char* device_name; //串口设备名
     int bud_rate;   //波特率
-    int init(const char* name, int bud);
+    int serial_state = SERIAL_OK;   //串口状态
+    int serialOpen();
+    int serialWrite(const char* data, int length);
+    int serialDataReady();
+    int serialRead(void *data_read, int length);
+    int serialGetChar();
+    void serialPrintf (const char *message, ...);
+    Usart(const char* name, int bud);
+    ~Usart();
 };
+
--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include "main.hpp"
 #include "usart.hpp"
+#include <thread>
 
 using namespace std;
+char readbuffer[10];
 
-Usart usart0;
+Usart usart0("/dev/ttyACM0", 115200);
 
 int main()
 {
-    usart0.init("/dev/ACM0", 115200);
-    std::cout << usart0.bud_rate << std::endl;
+    std::cout << usart0.device_name << std::endl;
+    while(1) 
+    {
+        usart0.serialPrintf("naive!");
+        //usart0.serialPrintf("%f\n", 114.514);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << usart0.serialDataReady() <<std::endl;
+        if(usart0.serialDataReady() > 0){
+            std::cout << usart0.serialGetChar() << std::endl;
+        }
+    }
     return 0;
 }
