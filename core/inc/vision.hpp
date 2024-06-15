21fc29d350a0f467d1c7e34debce2747912a5aed
FishSWA <zhonghengyu2@gmail.com>
2024-06-15 00:10:22 -0400

    整理代码&添加第二个摄像头

--- core/inc/vision.hpp
+++ core/inc/vision.hpp
@@ -7,8 +7,14 @@ private:
     int cam_l_id = 0;  //左摄像头id
     int cam_r_id = 0;  //右摄像头id
 
-    /*魔法参数*/
+    /*----------魔法参数------------*/
     float circle_ratio = 4*3.14; //圆(周长平方/面积)的比值
+    float circle_tolorance = 0.8; //判定圆允许的误差
+    /*过滤乒乓球颜色的阈值  (H, S, V)*/
+    cv::Scalar Filter_HSV_LOW = cv::Scalar(70, 100, 210);
+    cv::Scalar Filter_HSV_HIGH = cv::Scalar(170, 255, 255);
+    int Filter_Debug = 0;
+    
 
     /*摄像机参数*/
     int CAM_FRAME_HEIGHT = 480;
@@ -22,15 +28,20 @@ private:
 
     /*图像识别函数*/
     int hough_circles(cv::Mat frame_input);    //用霍夫变换识别圆
+    //识别乒乓球
+    int detect_balls(cv::InputArray frame_in, cv::InputOutputArray frame_labled,
+                        cv::OutputArray frame_binary, std::vector<cv::Point> &balls_detected);
     
 public:
     cv::VideoCapture Cam_L; //左摄像头对象
     cv::VideoCapture Cam_R; //右摄像头对象
-
     void init(int cam_l, int cam_r, int cam_dist);
     int open();     //打开相机
     int monitor_window();   //打开相机的监视窗口
-    int monitor_task();     //相机监视窗口打维护线程
+    int monitor_task();     //相机监视窗口的维护线程
+
+
+
     BinoCamera(int cam_l_in, int cam_r_in, int cam_dist_in);
     ~BinoCamera();
 };
--- core/src/vision.cpp
+++ core/src/vision.cpp
@@ -35,16 +35,76 @@ int BinoCamera::open()
     return 0;
 }
 
+int BinoCamera::detect_balls(cv::InputArray frame_in, cv::InputOutputArray frame_labled,
+                        cv::OutputArray frame_binary, std::vector<cv::Point> &balls_detected)
+{
+    std::vector<std::vector<cv::Point> > country;   //轮廓线
+    std::vector<cv::Vec4i> hiera;
+    cv::Rect bound_box;
+
+    /*转换为HSV色彩*/
+    frame_in.copyTo(frame_labled);
+    cv::cvtColor(frame_in, frame_binary, cv::COLOR_RGB2HSV);
+
+    /*Debug: 输出中心点的HVS值, 用来调过滤器阈值*/
+    if(Filter_Debug){
+        cv::circle(frame_labled, cv::Point(CAM_FRAME_WIDTH/2, CAM_FRAME_HEIGHT/2), 
+                                                        10, cv::Scalar(255,0,0));
+        std::cout << Frame_raw_R.at<cv::Vec3b>(CAM_FRAME_HEIGHT/2, CAM_FRAME_WIDTH/2) 
+                                                                    << std::endl;
+    }
+    
+    /*过滤&二值化*/
+    cv::inRange(frame_binary, Filter_HSV_LOW,
+                            Filter_HSV_HIGH, frame_binary);
+    cv::medianBlur(frame_binary, frame_binary, 3);
+    cv::threshold(frame_binary, frame_binary, 120, 255, cv::THRESH_BINARY);
+
+    /*寻找轮廓*/
+    cv::findContours(frame_binary, country, hiera, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+    /*筛选乒乓球的轮廓*/
+    //用面积大小 & 边长和面积比值筛选圆
+
+    balls_detected.clear();     //清空旧有的数据
+    for( size_t i = 0; i< country.size(); i++ )
+    {
+        /*计算轮廓的边缘&面积*/
+        float area, length; 
+        area = cv::contourArea(country[i]);
+        length = cv::arcLength(country[i], true);
+        /*当符合乒乓球轮廓的条件时*/
+        if(area > 100 && abs(length*length/area - circle_ratio) < circle_ratio*0.9)
+        {
+            cv::Scalar color = cv::Scalar(255, 0, 0);
+            /*画出轮廓&标记框&圆心*/
+            drawContours(frame_labled, country, (int)i, color, 2, cv::LINE_8, hiera, 0);
+            bound_box = cv::boundingRect(country[i]);
+            cv::rectangle(frame_labled, bound_box.tl(), bound_box.br(), cv::Scalar(0, 255, 0), 2);
+            cv::circle(frame_labled, cv::Point(bound_box.x+bound_box.width/2, 
+                                                bound_box.y+bound_box.height/2), 2, 
+                                                cv::Scalar(0, 0, 255), 2);
+            /*输出圆心数据*/
+            balls_detected.push_back(cv::Point(bound_box.x+bound_box.width/2, 
+                                        bound_box.y+bound_box.height/2));
+            // printf("center:%d, %d\n", bound_box.x+bound_box.width/2, bound_box.y+bound_box.height/2);
+        }
+    }
+
+
+    return 0;
+}
+
+
 int BinoCamera::monitor_task()
 {
     namedWindow("Cam_L", cv::WINDOW_AUTOSIZE);  //左摄像头窗口
     namedWindow("Cam_R", cv::WINDOW_AUTOSIZE);  //右摄像头窗口
 
-    cv::Mat frame_binary;
-    std::vector<std::vector<cv::Point> > country;   //轮廓线
-    std::vector<cv::Vec4i> hiera;
+    cv::Mat frame_binary[2];
+    cv::Mat frame_labeled[2];
+    std::vector<cv::Point> balls[2];
 
-    cv::Rect bound_box;
 
     while(1)
     {
@@ -52,42 +112,15 @@ int BinoCamera::monitor_task()
         Cam_R.read(Frame_raw_R);
 
         /*画面处理，暂时*/
-        
-        /*过滤颜色&二值化*/
-        cv::cvtColor(Frame_raw_R, frame_binary, cv::COLOR_RGB2HSV);
-        cv::circle(Frame_raw_R, cv::Point(CAM_FRAME_WIDTH/2, CAM_FRAME_HEIGHT/2), 10, cv::Scalar(255,0,0));
-        std::cout << Frame_raw_R.at<cv::Vec3b>(CAM_FRAME_HEIGHT/2, CAM_FRAME_WIDTH/2) << std::endl;
-        cv::inRange(frame_binary, cv::Scalar(85, 100, 210),
-                                cv::Scalar(110, 180, 255), frame_binary);
-        cv::medianBlur(frame_binary, frame_binary, 3);
-        cv::threshold(frame_binary, frame_binary, 120, 255, cv::THRESH_BINARY);
-
-        /*寻找轮廓*/
-        cv::findContours(frame_binary, country, hiera, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-
-        /*绘制边界*/
-        //用面积大小 & 边长和面积比值筛选圆
-        for( size_t i = 0; i< country.size(); i++ )
-        {
-            float area, length; //周长&面积
-            area = cv::contourArea(country[i]);
-            length = cv::arcLength(country[i], true);
-            if(area > 100 && abs(length*length/area - circle_ratio) < circle_ratio*0.9)
-            {
-                // printf("id:%d, aera:%f, length:%f\n", i, area, length);
-                cv::Scalar color = cv::Scalar(255, 0, 0);
-                drawContours( Frame_raw_R, country, (int)i, color, 2, cv::LINE_8, hiera, 0);
-                bound_box = cv::boundingRect(country[i]);
-                cv::rectangle( Frame_raw_R, bound_box.tl(), bound_box.br(), cv::Scalar(0, 255, 0), 2);
-                cv::circle(Frame_raw_R, cv::Point(bound_box.x+bound_box.width/2, 
-                                                    bound_box.y+bound_box.height/2), 2, 
-                                                    cv::Scalar(0, 0, 255), 2);
-                printf("center:%d, %d\n", bound_box.x+bound_box.width/2, bound_box.y+bound_box.height/2);
-            }
+        detect_balls(Frame_raw_L, frame_labeled[0], frame_binary[0], balls[0]);
+        detect_balls(Frame_raw_R, frame_labeled[1], frame_binary[1], balls[1]);
+        if(balls[0].size() > 0 && balls[1].size() > 0){
+            printf("centerL: %d, %d\t", balls[0][0].x, balls[0][0].y);
+            printf("centerR: %d, %d\n", balls[1][0].x, balls[1][0].y);
         }
 
-        cv::imshow("Cam_L", Frame_raw_R);
-        cv::imshow("Cam_R", frame_binary);
+        cv::imshow("Cam_L", frame_labeled[0]);
+        cv::imshow("Cam_R", frame_labeled[1]);
         if(cv::waitKey(1) == 'q') break;
 
     }
