e5d695b84f2a8fc4a5aa33211c76bf65be55df03
FishSWA <zhonghengyu2@gmail.com>
2024-06-19 02:37:01 +0800

    整理了一部分,但是乱码

--- core/inc/rknpu_yolo.hpp
+++ core/inc/rknpu_yolo.hpp
@@ -15,19 +15,53 @@
 #include "preprocess.h"
 
 
+#define INPUT_MAX_ATTR 1    //分配的输入张量数量
+#define OUTPUT_MAX_ARRT 3   //分配的输出张量数量
+
+
 class RkNPU
 {
 private:
-    /* data */
+    /*初始化参数*/
+    int model_data_size = 0;
+    unsigned char *model_data;
+    rknn_sdk_version version;
+    rknn_input_output_num io_num;
+    rknn_tensor_attr input_attrs[1];
+    rknn_tensor_attr output_attrs[3];
+    rknn_input inputs[1];
+    rknn_output outputs[OUTPUT_MAX_ARRT];
+    int channel = 3;
+    int width = 0;
+    int height = 0;
 
 
+    /* 内部参数 */
+    int error_ret;
+    rknn_context ctx;
+    size_t actual_size = 0;
+    int img_width = 0;
+    int img_height = 0;
+    int img_channel = 0;
+    const float nms_threshold = NMS_THRESH;      // 默认的NMS阈值
+    const float box_conf_threshold = BOX_THRESH; // 默认的置信度阈值
+    struct timeval start_time, stop_time;
+    char *model_name = "/home/fish/GKD/RK3588_FPV_Master/model/yolov5s-640-640.rknn";
+    char *input_path = "/home/fish/Downloads/rknpu2/examples/rknn_yolov5_demo/model/bus.jpg";
 
+    /* 内部工具函数 */
     double __get_us(struct timeval t) { return (t.tv_sec * 1000000 + t.tv_usec); }
 
 public:
     unsigned char *load_data(FILE *fp, size_t ofst, size_t sz);
     unsigned char *load_model(char *filename, int *model_size);
     void dump_tensor_attr(rknn_tensor_attr *attr);
+    int rknn_model_init();       //载入&初始化模型
+    int rknn_img_inference(cv::Mat& frame, _detect_result_group_t *results);    //图像推理
+    int yolo_draw_results(cv::Mat frame_in, cv::Mat& frame_labbed,
+                                 _detect_result_group_t *results);     //画出识别结果
+    int yolo_print_results(_detect_result_group_t *results);     //打印出识别结果
+
     int debug_main(); 
 
 
@@ -35,10 +69,4 @@ public:
     // ~RkNPU();
 };
 
-// RkNPU::RkNPU(/* args */)
-// {
-// }
 
-// RkNPU::~RkNPU()
-// {
-// }
--- core/src/rknpu_yolo.cpp
+++ core/src/rknpu_yolo.cpp
@@ -71,109 +71,58 @@ void RkNPU::dump_tensor_attr(rknn_tensor_attr *attr)
          get_qnt_type_string(attr->qnt_type), attr->zp, attr->scale);
 }
 
-
-int RkNPU::debug_main()
+int RkNPU::rknn_model_init()
 {
-  // if (argc < 2)
-  // {
-  //   printf("Usage: %s <rknn model> <input_image_path> <resize/letterbox> <output_image_path>\n", argv[0]);
-  //   return -1;
-  // }
-  int ret;
-  rknn_context ctx;
-  size_t actual_size = 0;
-  int img_width = 0;
-  int img_height = 0;
-  int img_channel = 0;
-  const float nms_threshold = NMS_THRESH;      // 默认的NMS阈值
-  const float box_conf_threshold = BOX_THRESH; // 默认的置信度阈值
-  struct timeval start_time, stop_time;
-  char *model_name = "/home/fish/GKD/RK3588_FPV_Master/model/yolov5s-640-640.rknn";
-  char *input_path = "/home/fish/Downloads/rknpu2/examples/rknn_yolov5_demo/model/bus.jpg";
-  std::string option = "letterbox";
-  std::string out_path = "./out.jpg";
-
-//   // init rga context
-//   rga_buffer_t src;
-//   rga_buffer_t dst;
-//   memset(&src, 0, sizeof(src));
-//   memset(&dst, 0, sizeof(dst));
-
-  printf("post process config: box_conf_threshold = %.2f, nms_threshold = %.2f\n", box_conf_threshold, nms_threshold);
-
-  /* Create the neural network */
-  printf("Loading mode...\n");
+  /*------载入模型-------*/
   int model_data_size = 0;
-  unsigned char *model_data = load_model(model_name, &model_data_size);
-  ret = rknn_init(&ctx, model_data, model_data_size, 0, NULL);
-  if (ret < 0)
-  {
-    printf("rknn_init error ret=%d\n", ret);
-    return -1;
-  }
+  model_data = load_model(model_name, &model_data_size);
+  error_ret = rknn_init(&ctx, model_data, model_data_size, 0, NULL);
+  if (error_ret < 0)  return error_ret;
 
-  rknn_sdk_version version;
-  ret = rknn_query(ctx, RKNN_QUERY_SDK_VERSION, &version, sizeof(rknn_sdk_version));
-  if (ret < 0)
-  {
-    printf("rknn_init error ret=%d\n", ret);
-    return -1;
-  }
-  printf("sdk version: %s driver version: %s\n", version.api_version, version.drv_version);
 
-  rknn_input_output_num io_num;
-  ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
-  if (ret < 0)
-  {
-    printf("rknn_init error ret=%d\n", ret);
-    return -1;
-  }
-  printf("model input num: %d, output num: %d\n", io_num.n_input, io_num.n_output);
+  /*------查询信息------*/
+  /*查询版本*/
+  error_ret = rknn_query(ctx, RKNN_QUERY_SDK_VERSION, &version, sizeof(rknn_sdk_version));
+  if (error_ret < 0)  return error_ret;
 
-  rknn_tensor_attr input_attrs[io_num.n_input];
+  /*输入&输出的维度*/
+  error_ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
+  if (error_ret < 0)  return error_ret;
+  // printf("in:%d, ou:%d\n", io_num.n_input, io_num.n_output);
+
+  /*输入向量*/
   memset(input_attrs, 0, sizeof(input_attrs));
   for (int i = 0; i < io_num.n_input; i++)
   {
     input_attrs[i].index = i;
-    ret = rknn_query(ctx, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
-    if (ret < 0)
-    {
-      printf("rknn_init error ret=%d\n", ret);
-      return -1;
-    }
-    dump_tensor_attr(&(input_attrs[i]));
+    error_ret = rknn_query(ctx, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
+    if (error_ret < 0)  return error_ret;
+    // dump_tensor_attr(&(input_attrs[i]));
   }
 
-  rknn_tensor_attr output_attrs[io_num.n_output];
+  /*输出向量*/
   memset(output_attrs, 0, sizeof(output_attrs));
   for (int i = 0; i < io_num.n_output; i++)
   {
     output_attrs[i].index = i;
-    ret = rknn_query(ctx, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
-    dump_tensor_attr(&(output_attrs[i]));
+    error_ret = rknn_query(ctx, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
+    if (error_ret < 0)  return error_ret;
+    // dump_tensor_attr(&(output_attrs[i]));
   }
 
-  int channel = 3;
-  int width = 0;
-  int height = 0;
-  if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
+  if (input_attrs[0].fmt == RKNN_TENSOR_NCHW) //NCHW input fmt
   {
-    printf("model is NCHW input fmt\n");
     channel = input_attrs[0].dims[1];
     height = input_attrs[0].dims[2];
     width = input_attrs[0].dims[3];
   }
-  else
+  else    //HWC input fmt
   {
-    printf("model is NHWC input fmt\n");
     height = input_attrs[0].dims[1];
     width = input_attrs[0].dims[2];
     channel = input_attrs[0].dims[3];
   }
 
-  printf("model input height=%d, width=%d, channel=%d\n", height, width, channel);
-
-  rknn_input inputs[1];
   memset(inputs, 0, sizeof(inputs));
   inputs[0].index = 0;
   inputs[0].type = RKNN_TENSOR_UINT8;
@@ -181,6 +130,72 @@ int RkNPU::debug_main()
   inputs[0].fmt = RKNN_TENSOR_NHWC;
   inputs[0].pass_through = 0;
 
+  return 0;
+}
+
+int RkNPU::rknn_img_inference(cv::Mat& frame, _detect_result_group_t *results)
+{
+
+  cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+  img_width = frame.cols;
+  img_height = frame.rows;
+
+  // 指定目标大小和预处理方式,默认使用LetterBox的预处理
+  BOX_RECT pads;
+  memset(&pads, 0, sizeof(BOX_RECT));
+  cv::Size target_size(width, height);
+  cv::Mat resized_img(target_size.height, target_size.width, CV_8UC3);
+  // 计算缩放比例
+  float scale_w = (float)target_size.width / frame.cols;
+  float scale_h = (float)target_size.height / frame.rows;
+
+  if (img_width != width || img_height != height)
+  {
+      // printf("resize image with letterbox\n");
+      float min_scale = std::min(scale_w, scale_h);
+      scale_w = min_scale;
+      scale_h = min_scale;
+      letterbox(frame, resized_img, pads, min_scale, target_size);
+    inputs[0].buf = resized_img.data;
+  }
+  else
+  {
+    inputs[0].buf = frame.data;
+  }
+
+  gettimeofday(&start_time, NULL);
+  rknn_inputs_set(ctx, io_num.n_input, inputs);
+
+  
+  memset(outputs, 0, sizeof(outputs));
+  for (int i = 0; i < io_num.n_output; i++)
+  {
+    outputs[i].want_float = 0;
+  }
+
+  // 执行推理
+  error_ret = rknn_run(ctx, NULL);
+  error_ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
+
+  // 后处理
+  std::vector<float> out_scales;
+  std::vector<int32_t> out_zps;
+  for (int i = 0; i < io_num.n_output; ++i)
+  {
+    out_scales.push_back(output_attrs[i].scale);
+    out_zps.push_back(output_attrs[i].zp);
+  }
+  post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
+                box_conf_threshold, nms_threshold, pads, scale_w, scale_h, out_zps, out_scales, results);
+
+  return 0;
+}
+
+int RkNPU::debug_main()
+{
+
+  error_ret = rknn_model_init();  //初始化RKNN模型
+  if(error_ret < 0) printf("model_init_err, id:%d\n", error_ret);
 
   ///////////////////////////////////////////
 
@@ -196,106 +211,22 @@ int RkNPU::debug_main()
   Cam.set(cv::CAP_PROP_BRIGHTNESS, 10);
   Cam.set(cv::CAP_PROP_TEMPERATURE, 7000);
 
-  Cam.read(frame_cam);
-
   cv::namedWindow("yolo_cam", cv::WINDOW_AUTOSIZE);
 
   while(1){
   
   Cam.read(frame_cam);
+  
+  _detect_result_group_t detect_result;
 
-  if (!frame_cam.data)
-  {
-    printf("cv::imread %s fail!\n", input_path);
-    return -1;
-  }
-  cv::Mat img;
-  cv::cvtColor(frame_cam, img, cv::COLOR_BGR2RGB);
-  img_width = img.cols;
-  img_height = img.rows;
-  // printf("img width = %d, img height = %d\n", img_width, img_height);
+  rknn_img_inference(frame_cam, &detect_result);
 
-  // 指定目标大小和预处理方式,默认使用LetterBox的预处理
-  BOX_RECT pads;
-  memset(&pads, 0, sizeof(BOX_RECT));
-  cv::Size target_size(width, height);
-  cv::Mat resized_img(target_size.height, target_size.width, CV_8UC3);
-  // 计算缩放比例
-  float scale_w = (float)target_size.width / img.cols;
-  float scale_h = (float)target_size.height / img.rows;
-
-  if (img_width != width || img_height != height)
-  {
-    // 直接缩放采用RGA加速
-    if (option == "resize")
-    {
-    //   // printf("resize image by rga\n");
-    //   ret = resize_rga(src, dst, img, resized_img, target_size);
-    //   if (ret != 0)
-    //   {
-    //     // fprintf(stderr, "resize with rga error\n");
-    //     return -1;
-    //   }
-    //   // 保存预处理图片
-    //   //cv::imwrite("resize_input.jpg", resized_img);
-    }
-    else if (option == "letterbox")
-    {
-      // printf("resize image with letterbox\n");
-      float min_scale = std::min(scale_w, scale_h);
-      scale_w = min_scale;
-      scale_h = min_scale;
-      letterbox(img, resized_img, pads, min_scale, target_size);
-      // 保存预处理图片
-      //cv::imwrite("letterbox_input.jpg", resized_img);
-    }
-    else
-    {
-      // fprintf(stderr, "Invalid resize option. Use 'resize' or 'letterbox'.\n");
-      return -1;
-    }
-    inputs[0].buf = resized_img.data;
-  }
-  else
-  {
-    inputs[0].buf = img.data;
-  }
-
-  gettimeofday(&start_time, NULL);
-  rknn_inputs_set(ctx, io_num.n_input, inputs);
-
-  rknn_output outputs[io_num.n_output];
-  memset(outputs, 0, sizeof(outputs));
-  for (int i = 0; i < io_num.n_output; i++)
-  {
-    outputs[i].want_float = 0;
-  }
-
-  // 执行推理
-  ret = rknn_run(ctx, NULL);
-  ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
-
-
-
-  /////////////////////
-
-  // 后处理
-  detect_result_group_t detect_result_group;
-  std::vector<float> out_scales;
-  std::vector<int32_t> out_zps;
-  for (int i = 0; i < io_num.n_output; ++i)
-  {
-    out_scales.push_back(output_attrs[i].scale);
-    out_zps.push_back(output_attrs[i].zp);
-  }
-  post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
-               box_conf_threshold, nms_threshold, pads, scale_w, scale_h, out_zps, out_scales, &detect_result_group);
 
   // 画框和概率
   char text[256];
-  for (int i = 0; i < detect_result_group.count; i++)
+  for (int i = 0; i < detect_result.count; i++)
   {
-    detect_result_t *det_result = &(detect_result_group.results[i]);
+    detect_result_t *det_result = &(detect_result.results[i]);
     sprintf(text, "%s %.1f%%", det_result->name, det_result->prop * 100);
     printf("%s @ (%d %d %d %d) %f\n", det_result->name, det_result->box.left, det_result->box.top,
            det_result->box.right, det_result->box.bottom, det_result->prop);
@@ -307,10 +238,7 @@ int RkNPU::debug_main()
     putText(frame_cam, text, cv::Point(x1, y1 + 12), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255));
   }
 
-
-  // printf("save detect result to %s\n", out_path.c_str());
-  // imwrite(out_path, frame_cam);
-  ret = rknn_outputs_release(ctx, io_num.n_output, outputs);
+  error_ret = rknn_outputs_release(ctx, io_num.n_output, outputs);
 
   cv::imshow("yolo_cam", frame_cam);
 
@@ -319,31 +247,6 @@ int RkNPU::debug_main()
   gettimeofday(&stop_time, NULL);
   printf("once run use %f ms\n", (__get_us(stop_time) - __get_us(start_time)) / 1000);
   }
-  ///////////////////////////
-
-
-//   // 耗时统计
-//   int test_count = 10;
-//   gettimeofday(&start_time, NULL);
-//   for (int i = 0; i < test_count; ++i)
-//   {
-//     rknn_inputs_set(ctx, io_num.n_input, inputs);
-//     ret = rknn_run(ctx, NULL);
-//     ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
-// #if PERF_WITH_POST
-//     post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
-//                  box_conf_threshold, nms_threshold, pads, scale_w, scale_h, out_zps, out_scales, &detect_result_group);
-// #endif
-//     ret = rknn_outputs_release(ctx, io_num.n_output, outputs);
-//   }
-//   gettimeofday(&stop_time, NULL);
-//   printf("loop count = %d , average run  %f ms\n", test_count,
-//          (__get_us(stop_time) - __get_us(start_time)) / 1000.0 / test_count);
-
-//   deinitPostProcess();
-
-//   // release
-//   ret = rknn_destroy(ctx);
 
   if (model_data)
   {
