433642bd7b160733190b8fc319742411b68c097a
FishSWA <zhonghengyu2@gmail.com>
2024-06-18 23:37:45 +0800

    添加了rknpu需要的库文件

---
+++ core/src/rknpu_yolo.cpp
@@ -0,0 +1,53 @@
+#include "rknpu_yolo.hpp"
+
+unsigned char *RkNPU::load_data(FILE *fp, size_t ofst, size_t sz)
+{
+  unsigned char *data;
+  int ret;
+
+  data = NULL;
+
+  if (NULL == fp)
+  {
+    return NULL;
+  }
+
+  ret = fseek(fp, ofst, SEEK_SET);
+  if (ret != 0)
+  {
+    printf("blob seek failure.\n");
+    return NULL;
+  }
+
+  data = (unsigned char *)malloc(sz);
+  if (data == NULL)
+  {
+    printf("buffer malloc failure.\n");
+    return NULL;
+  }
+  ret = fread(data, 1, sz, fp);
+  return data;
+}
+
+unsigned char *RkNPU::load_model(char *filename, int *model_size)
+{
+  FILE *fp;
+  unsigned char *data;
+
+  fp = fopen(filename, "rb");
+  if (NULL == fp)
+  {
+    printf("Open file %s failed.\n", filename);
+    return NULL;
+  }
+
+  fseek(fp, 0, SEEK_END);
+  int size = ftell(fp);
+
+  data = load_data(fp, 0, size);
+
+  fclose(fp);
+
+  *model_size = size;
+  return data;
+}
